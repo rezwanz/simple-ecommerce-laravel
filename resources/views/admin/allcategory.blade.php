755e71394158f5f2271e7a6ef748b03cdb9ad597
rezwan <mrm.shakil@yahoo.com>
2023-06-14 12:16:22 +0600

    Add and All category view file done

--- resources/views/admin/allcategory.blade.php
+++ resources/views/admin/allcategory.blade.php
@@ -4,6 +4,36 @@
 @endsection
 @section('content')
 
-    Hello from All Category
+    <div class="container-xxl flex-grow-1 container-p-y">
+        <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Page/</span> All Category</h4>
+        <div class="card">
+            <h5 class="card-header">Available Category List</h5>
+            <div class="table-responsive text-nowrap">
+                <table class="table">
+                    <thead class="table-light">
+                    <tr>
+                        <th>ID</th>
+                        <th>Category Name</th>
+                        <th>Sub Category</th>
+                        <th>Product</th>
+                        <th>Actions</th>
+                    </tr>
+                    </thead>
+                    <tbody class="table-border-bottom-0">
+                    <tr>
+                        <td>1</td>
+                        <td>Electronics</td>
+                        <td>10</td>
+                        <td>100</td>
+                        <td>
+                            <a href="edit" class="btn btn-primary">Edit</a>
+                            <a href="delete" class="btn btn-danger">Delete</a>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 
 @endsection
