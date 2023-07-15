f5df2490fd4bfb0f4e0dfd92a9fb9a5a0cb5da95
rezwan <mrm.shakil@yahoo.com>
2023-07-15 15:20:44 +0600

    Category modified

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -3,17 +3,33 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
     public function Index()
     {
-        return view('admin.allcategory');
+        $categories = category::latest()->get();
+        return view('admin.allcategory', compact('categories'));
     }
 
     public function AddCategory()
     {
         return view('admin.addcategory');
+    }
+
+    public function StoreCategory(Request $request)
+    {
+        $request->validate([
+            'category_name' => 'required|unique:categories'
+        ]);
+
+        Category::insert([
+            'category_name' => $request->category_name,
+            'slug' => strtolower(str_replace(' ','-',$request->category_name))
+        ]);
+
+        return redirect()->route('allcategory')->with('message', 'Category Added Successfully!');
     }
 }
--- resources/views/admin/allcategory.blade.php
+++ resources/views/admin/allcategory.blade.php
@@ -8,6 +8,11 @@
         <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Page/</span> All Category</h4>
         <div class="card">
             <h5 class="card-header">Available Category List</h5>
+            @if(session()->has('message'))
+                <div class="alert alert-success">
+                    {{ session()->get('message') }}
+                </div>
+            @endif
             <div class="table-responsive text-nowrap">
                 <table class="table">
                     <thead class="table-light">
@@ -20,16 +25,18 @@
                     </tr>
                     </thead>
                     <tbody class="table-border-bottom-0">
+                    @foreach($categories as $category)
                     <tr>
-                        <td>1</td>
-                        <td>Electronics</td>
-                        <td>10</td>
-                        <td>100</td>
+                        <td>{{ $category->id }}</td>
+                        <td>{{ $category->category_name }}</td>
+                        <td>{{ $category->category_count }}</td>
+                        <td>{{ $category->slug }}</td>
                         <td>
                             <a href="edit" class="btn btn-primary">Edit</a>
                             <a href="delete" class="btn btn-danger">Delete</a>
                         </td>
                     </tr>
+                    @endforeach
                     </tbody>
                 </table>
             </div>
